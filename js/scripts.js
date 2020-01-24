78314b5ea34b3dcaa674af5d70a90f94b31f5b24
neha doodi <nehadoodipoonia@gmail.com>
2020-01-24 10:09:33 -0800

    add else if condition when input number has '1'

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,19 @@ $(document).ready(function() {
         for (var i=0; i<=array.length;i++){
             if (array[i]==="3") {
                 var result = "I'm sorry, Dave.I'm afraid I can't do that";
-                console.log(1);
                 array = result;
-                console.log(2);
                 alert(array);
+                break;
+            } else if (array[i]==="2") {
+                var result = "Boop!";
+                array = result;
+                alert(array);
+                break;
+            } else if (array[i] === "1") {
+                var result = "Beep!";
+                array = result;
+                alert(result);
+                break;
             }
         }
     });
