8791bd6966bf6ef2630a0ce1b025575dca21d0e4
neha doodi <nehadoodipoonia@gmail.com>
2020-01-24 15:53:39 -0800

    add function for larger numbers, add image

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
+  
     $("form#formOne").submit(function(event) {
         event.preventDefault();
-        var array = $("input#number").val();
-        for (var i=0; i<=array.length;i++){
-            if (array[i]==="3") {
-                var result = "I'm sorry, Dave.I'm afraid I can't do that";
-                array = result;
-                alert(array);
-                break;
-            } else if (array[i]==="2") {
-                var result = "Boop!";
-                array = result;
-                alert(array);
-                break;
-            } else if (array[i] === "1") {
-                var result = "Beep!";
-                array = result;
-                alert(result);
-                break;
-            }
+        var number = $("input#number").val();
+        var result = "";
+        for (var i =0; i<=number; i++) {
+           var transformOutput  = transform(i);
+           result = result.concat(transformOutput).concat(", ");            
         }
+        result = result.substring(0,result.length-2);
+        $("#result").text(result);
+        $("#image").show();
+
     });
+
+    function transform(input) {
+        var output;
+        if (input.toString().includes("3")===true) {
+            output = "I'm sorry, Dave. I'm afaraid I can't do that.";
+        } else if (input.toString().includes("2")===true) {
+            output = "Beep!";
+        } else if (input.toString().includes("1")===true){
+            output = "Boop!";
+        } else {
+            output= input;
+        }
+        return output;
+    }
 });
